6d53c5f8d8125ca6c8720d6820065f653ff7f7f5
kiran-classet <kiran.m@classet.in>
2025-03-05 12:30:34 +0530

    dynamic icon change based on date change

--- android/app/src/main/kotlin/com/example/classet_admin/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/classet_admin/MainActivity.kt
@@ -1,5 +1,24 @@
 package com.example.classet_admin
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import androidx.annotation.NonNull
+import android.util.Log
+import com.example.classet_admin.IconManager
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(@NonNull savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("MainActivity", "onCreate called")
+        updateIcon()
+    }
+
+    private fun updateIcon() {
+        try {
+            IconManager(this).updateAppIcon()
+        } catch (e: Exception) {
+            Log.e("MainActivity", "Error updating icon", e)
+            e.printStackTrace()
+        }
+    }
+}
