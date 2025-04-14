10c9ecef2e550a5527a23912e7f3c52a1775170c
kiran-classet <kiran.m@classet.in>
2025-04-14 21:21:16 +0530

    feat: enhance student status change approval screen with loading states and approval actions

--- lib/features/approvals/views/student_status_change_approval_screen.dart
+++ lib/features/approvals/views/student_status_change_approval_screen.dart
@@ -1,6 +1,5 @@
 import 'package:classet_admin/core/providers/filter_provider.dart';
 import 'package:classet_admin/features/auth/providers/admin_user_provider.dart';
-import 'package:classet_admin/features/dashboard/views/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:classet_admin/core/widgets/filter_button_widget.dart';
@@ -17,6 +16,8 @@ class StudentStatusChangeApprovalScreen extends ConsumerStatefulWidget {
 class _StudentStatusChangeApprovalScreenState
     extends ConsumerState<StudentStatusChangeApprovalScreen> {
   Map<String, dynamic>? _userAssignedDetails; // State to store API response
+  List<dynamic> _approvals = []; // State to store approvals list
+  bool _isLoading = false; // State to manage loader visibility
 
   @override
   void initState() {
@@ -25,42 +26,55 @@ class _StudentStatusChangeApprovalScreenState
   }
 
   Future<void> _fetchUserAssignedDetails() async {
+    setState(() {
+      _isLoading = true; // Show loader
+    });
     try {
       final apiService = ApiService();
       final response = await apiService.get(
           'settings-approvals/userAssignedBoards?approvalType=studentStatusChange');
       if (response['status'] == true) {
         setState(() {
+          _isLoading = false; // Hide loader
           _userAssignedDetails = response['data']; // Save response in state
         });
         print('User Assigned Details: $_userAssignedDetails');
       } else {
+        setState(() {
+          _isLoading = false; // Hide loader
+        });
         print('Failed to fetch user assigned details: ${response['message']}');
       }
     } catch (e) {
       print('Error fetching user assigned details: $e');
+    } finally {
+      setState(() {
+        _isLoading = false; // Hide loader
+      });
     }
   }
 
   Future<void> _fetchPendingApprovals() async {
     if (_userAssignedDetails == null) return;
-    // setState(() {
-    //   _isLoading = true; // Show loader
-    // });
-    final filterState = ref.read(filterStateProvider); // Access saved filters
+
+    setState(() {
+      _isLoading = true; // Show loader
+    });
+
+    final filterState = ref.read(filterStateProvider);
     final sectionId =
         filterState.section.isNotEmpty ? filterState.section[0] : null;
 
     if (sectionId == null) {
-      // setState(() {
-      //   _students.clear();
-      //   _isLoading = false;
-      // });
+      setState(() {
+        _isLoading = false; // Hide loader
+      });
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('No section selected in filters')),
       );
       return;
     }
+
     final assignedHeirarchy =
         _userAssignedDetails!['adminUserAssignedLevelDetails'];
     final branchIds =
@@ -87,12 +101,70 @@ class _StudentStatusChangeApprovalScreenState
         payload,
       );
       if (response['status'] == true) {
-        print('Pending Approvals: ${response['data']}');
+        setState(() {
+          _isLoading = false; // Hide loader
+          _approvals = response['data']; // Save approvals in state
+        });
       } else {
         print('Failed to fetch pending approvals: ${response['message']}');
       }
     } catch (e) {
       print('Error fetching pending approvals: $e');
+    } finally {
+      setState(() {
+        _isLoading = false; // Hide loader
+      });
+    }
+  }
+
+  Future<void> _updateApprovalStatus(
+      Map<String, dynamic> approvalData, String approveStatus) async {
+    if (_userAssignedDetails == null) return;
+
+    setState(() {
+      _isLoading = true; // Show loader
+    });
+    print(
+        'User Assigned Details: ${_userAssignedDetails!['adminUserAssignedLevelDetails']}');
+    print('Approval Data Branch ID: ${approvalData['branchId']}');
+
+    final filterData = _userAssignedDetails!['adminUserAssignedLevelDetails']
+        .where((element) => element['_id'] == approvalData['branchId'])
+        .toList();
+
+    final payload = {
+      ...approvalData,
+      "approveStatus": approveStatus,
+      "statusChangeRemarks": approvalData['statusChangeRemarks'] ?? "",
+      "handledBy": approvalData['handledBy'] ?? [],
+      "levelAndBranchDetails": filterData,
+    };
+    try {
+      final apiService = ApiService();
+      final response = await apiService.post(
+        'statusChange/update',
+        payload,
+      );
+      if (response['status'] == true) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Status updated successfully')),
+        );
+        _fetchPendingApprovals();
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+              content: Text(
+                  'Failed to ${approveStatus.toLowerCase()}: ${response['message']}')),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error: $e')),
+      );
+    } finally {
+      setState(() {
+        _isLoading = false; // Hide loader
+      });
     }
   }
 
@@ -102,11 +174,87 @@ class _StudentStatusChangeApprovalScreenState
       appBar: AppBar(
         title: const Text('Student Status Change'),
       ),
-      body: Center(
-        child: _userAssignedDetails == null
-            ? const CircularProgressIndicator() // Show loader until data is fetched
-            : const Text('Student Status Change Approval Screen'),
-      ),
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator()) // Show loader
+          : _userAssignedDetails == null
+              ? const Center(child: CircularProgressIndicator())
+              : _approvals.isEmpty
+                  ? const Center(child: Text('No pending approvals'))
+                  : ListView.builder(
+                      itemCount: _approvals.length,
+                      itemBuilder: (context, index) {
+                        final approval = _approvals[index];
+                        return Card(
+                          margin: const EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 8),
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  approval['given_name'] ?? 'Unknown',
+                                  style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                const SizedBox(height: 8),
+                                Text(
+                                  'username: ${approval['username']?.substring(3) ?? 'N/A'}',
+                                  style: const TextStyle(fontSize: 14),
+                                ),
+                                Text(
+                                  'Branch: ${approval['branchName'] ?? 'N/A'}',
+                                  style: const TextStyle(fontSize: 14),
+                                ),
+                                Text(
+                                  'Section: ${approval['sectionName'] ?? 'N/A'}',
+                                  style: const TextStyle(fontSize: 14),
+                                ),
+                                Text(
+                                  'Parent Contact: ${approval['parentContactNo'] ?? 'N/A'}',
+                                  style: const TextStyle(fontSize: 14),
+                                ),
+                                Text(
+                                  'Remarks: ${approval['statusChangeRemarks'] ?? 'N/A'}',
+                                  style: const TextStyle(fontSize: 14),
+                                ),
+                                Text(
+                                  'Requested By: ${approval['requestRaisedByName']?.substring(3) ?? 'N/A'}',
+                                  style: const TextStyle(fontSize: 14),
+                                ),
+                                Text(
+                                  'Request Date: ${DateTime.parse(approval['createdAt']).toLocal()}',
+                                  style: const TextStyle(fontSize: 14),
+                                ),
+                                const SizedBox(height: 12),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    IconButton(
+                                      icon: const Icon(Icons.check,
+                                          color: Colors.green),
+                                      onPressed: () {
+                                        _updateApprovalStatus(
+                                            approval, "APROVE");
+                                      },
+                                    ),
+                                    IconButton(
+                                      icon: const Icon(Icons.close,
+                                          color: Colors.red),
+                                      onPressed: () {
+                                        _updateApprovalStatus(
+                                            approval, "REJECT");
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
       floatingActionButton: Container(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(30),
